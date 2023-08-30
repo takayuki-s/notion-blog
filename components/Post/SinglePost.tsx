089a0d349b9d7702e77a72eb05bd26ccb5972977
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-08-30 09:24:34 +0900

    ハイドレーションエラー修正

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -12,9 +12,10 @@ type Props = {
 const SinglePost = (props: Props) => {
   const { title, description, date, tags, slug } = props
   return (
-    <Link href={`/posts/${slug}`}>
+    <>
       <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
         <div className="flex items-center gap-3">
+          <Link href={`/posts/${slug}`}></Link>
           <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
           <div className="text-gray-100">{date}</div>
           {tags.map((tag) => (
@@ -25,7 +26,7 @@ const SinglePost = (props: Props) => {
         </div>
         <p className="text-gray-100">{description}</p>
       </section>
-    </Link>
+    </>
   )
 }
 
