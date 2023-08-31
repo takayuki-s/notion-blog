a5a8d3c113b2e11acfa1076225ef5a63fd12be61
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-09-01 06:46:52 +0900

    詳細ページルーティング

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -15,8 +15,9 @@ const SinglePost = (props: Props) => {
     <>
       <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
         <div className="flex items-center gap-3">
-          <Link href={`/posts/${slug}`}></Link>
-          <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
+          <Link href={`/posts/${slug}`}>
+            <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
+          </Link>
           <div className="text-gray-100">{date}</div>
           {tags.map((tag) => (
             <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium">
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Post = () => {
+  return <div>詳細記事ページ</div>
+}
+
+export default Post
