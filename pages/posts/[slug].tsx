8ffb82428d4234c1dea68208ac372dfbb29def01
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-09-02 10:30:11 +0900

    詳細ページレイアウト

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Post = () => {
-  return <div>詳細記事ページ</div>
+  return (
+    <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
+      <h2 className="w-full text-2xl font-medium">3回目の投稿です</h2>
+      <div className="border-b-2 w-1/3 mt-1 border-sky-700"></div>
+      <span className="text-gray-500">2023/09/02</span>
+      <br />
+      <p className="text-white bg-sky-700 rounded-xl font-medium mt-2 px-2 inline-block">
+        Next.js
+      </p>
+      <div className="mt-10 font-medium">
+        サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+      </div>
+    </section>
+  )
 }
 
 export default Post
